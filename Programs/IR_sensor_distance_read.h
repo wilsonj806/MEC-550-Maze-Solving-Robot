da5b56780f93b4535972a4a3d67599bb640cebcc
Wilson <wilsonj806@gmail.com>
2015-05-03 00:01:56 -0400

    Create IR_sensor_distance_read.h

---
+++ Programs/IR_sensor_distance_read.h
@@ -0,0 +1,34 @@
+/* 
+*********************************
+MEC 550 Maze Solving Robot
+IR sensor distance read
+May 02, 2015 Version 1
+*********************************
+
+************************
+Change History
+************************
+5/2/2015- Version 1
+-Created base version
+************************
+End Change History
+************************
+
+This program includes the calibration curve for the IR sensor
+
+*/
+
+#define irsensor 0
+
+void setup(){
+}
+void loop(){
+}
+
+int distanceRead(float raw){
+raw = analogRead(irsensor);
+float val = 0.0049 * raw;
+int distance = 1 * pow(10,-14)*pow(val,6)-pow(2,-11)*pow(val,5)
++pow(1,-18)*pow(val,4)-pow(4,-6)*pow(val,3)+pow(8,-4)*pow(val,2)+pow(-8.77,-2)*val+4.97;
+return distance;
+}
