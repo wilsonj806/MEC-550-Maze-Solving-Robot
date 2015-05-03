4d37ab4adaf43fd1e0f00210b1e7f0077061d6aa
Wilson <wilsonj806@gmail.com>
2015-05-03 00:18:47 -0400

    Update Dual_IR_sensor_raw.h

--- Programs/Dual_IR_sensor_raw.h
+++ Programs/Dual_IR_sensor_raw.h
@@ -1,49 +1,99 @@
 /*
- ******************************
+ ********************************
    MEC 550 Maze Solving Robot
-     Dual IR Sensor Read
-    Version 1 May 2, 2015
- ******************************
+     Color Reading Program
+    Version 2 May 2, 2015
+ *******************************
  
- *********************
-    Change History
- *********************
- Version 1 - 5/2/15
- - Created base version
- 
- **********************
-   End Change History
- **********************
- 
- This program reads voltage from two IR sensors
- One sensor is placed on the right, the other on the left
- 
- */
- 
-// define the infrared sensor input pin on the Arduino
-// Also note that "#define" does not actually use memory, very handy
-#define infraredPin1 0
-#define infraredPin2 2
-int navmatrix[20][20];
-// define the output of the infrared as a floating point variable
-int raw1, raw2;
-float val1, val2;
+ ********************************
+        Change History
+ ********************************
+Version 1 4/22/15
+- Created base version of the program
+- Need to annotate the program with comments more
+Version 2 5/2/15
+- Optimized dynamic memory use (granted it was 1% before, but still)
+ * Done by changing variables S0, S1, S2, S3 from "int" variables to "#define" variables
+ *********************************
+        End Change History
+ *********************************
+*/
+
+#define S0 8
+#define S1 9
+#define S2 11
+#define S3 12
+char outputPin = 10;
+#define LED 13
 
 void setup(){
-  Serial.begin(9600);
+  pinsetup();
+
 }
 
-void loop(){
-  raw1 = analogRead(infraredPin1);
-  raw2 = analogRead(infraredPin2);
-  // remember that you have to multiply the analogRead() value by 0.0049 due to the change from 10 bit to a voltage scheme
-  // resolution = (5V-0V)/(2^10) is approximately 0.0049 V
-  val1 = raw1*0.0049;
-  val2 = raw2*0.0049;
-  Serial.println("Sensor 1 raw voltage value: " );
-  Serial.println(val1);
-  Serial.println("Sensor 2 raw voltage value: ");
-  Serial.println(val2);
-  Serial.println("  ");
-  delay(1000);
+void loop() {
+detectColor(outputPin);
+delay(1000);
+}
+
+int detectColor(char outputPin){
+  int red = colorRead(outputPin, 1, 1); // detect % red
+  int green = colorRead(outputPin, 2, 1); // detect % green
+}
+int colorRead(char outputPin, int color, boolean LEDstate){ // declare color read function as a float
+  outputMode(1);
+  if(color == 1){
+    digitalWrite(S3, LOW);
+    digitalWrite(S2, LOW);
+  }
+  else if(color == 2){
+  digitalWrite(S3, HIGH);
+  digitalWrite(S2, HIGH);
+  }
+  float pulseTime; // declare a time for pulse arrival at the "emitter"
+  // turn LED on or off
+  if(LEDstate == 0){
+    digitalWrite(LED, LOW);
+  }
+ if(LEDstate == 1){
+   digitalWrite(LED, HIGH);
+ }
+ delay(100); // wait for LED to turn on
+ 
+ pulseTime = pulseIn(outputPin, LOW, 80000); // read low voltage pulse from output pin, timeout at 80000
+ if(pulseTime <.1){
+   pulseTime = 80000;
+ }
+ outputMode(0);
+ return pulseTime; // return the pulse time to the function that calls on colorRead()
+}
+
+
+// Set up the output pin frequency sensitivity
+void outputMode(char mode){
+  if(mode == 0){
+    digitalWrite(LED, LOW);
+    digitalWrite(S0, LOW);
+    digitalWrite(S1, LOW);
+  }
+  else if (mode == 1){
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, HIGH);
+  }
+  return;
+}
+    
+// Setting the pin modes
+void pinsetup(){
+  /* initialize pins/ set all pins but the outputPin as OUTPUT
+  S2, S3 is for color selection
+  S0, S1 is for communication frequency selection
+  */
+  pinMode(LED, OUTPUT); 
+  pinMode(S2, OUTPUT); 
+  pinMode(S3, OUTPUT);
+  pinMode(outputPin, INPUT);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  return;
 }
